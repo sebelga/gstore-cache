296726c1efb5a4c742acf5e2a3f32abd8164c145
sebelga <sabee77@gmail.com>
2018-02-04 14:50:23 +0100

    Initial commit

---
+++ test/index.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('gstore-cache', () => {
+    describe('init()', () => {
+        it('should work', () => {
+            expect(true).equal(true);
+        });
+    });
+});
