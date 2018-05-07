2af3410f66237ef594cd2b7753f3f86908f90e10
sebelga <sabee77@gmail.com>
2018-05-07 18:42:42 +0200

    fix(dsKeyToString): Update key to string conversion
    #4

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -17,8 +17,8 @@ describe('utils', () => {
             const str1 = dsKeyToString(key1, { hash: false });
             const str2 = dsKeyToString(key5, { hash: false });
 
-            expect(str1).equal('nsUser111');
-            expect(str2).equal('GranDadJohnDadMickUser555');
+            expect(str1).equal('ns:%:User:%:111');
+            expect(str2).equal('GranDad:%:John:%:Dad:%:Mick:%:User:%:555');
         });
 
         it('should throw an error if no Key passed', () => {
@@ -28,16 +28,23 @@ describe('utils', () => {
     });
 
     describe('dsQueryToString', () => {
+        const separator = ':%:';
         it('should convert the query to string', () => {
             const str1 = dsQueryToString(query1, { hash: false });
             const str2 = dsQueryToString(query2, { hash: false });
             const str3 = dsQueryToString(query3, { hash: false });
 
             expect(str1).equal(
-                'Company|com.domain.dev|name=Sympresafield1<123field2>789__key__HAS_ANCESTORParent123|field1field2|10|5|size-|namesize|X|Y' // eslint-disable-line
+                `Company${separator}com.domain.dev${separator}name=Sympresafield1<123field2>789__key__HAS_ANCESTORParent${separator}123${separator}field1field2${separator}10${separator}5${separator}size-${separator}namesize${separator}X${separator}Y` // eslint-disable-line
             );
-            expect(str2).equal('User||name=john||-1|-1|phone+|||');
-            expect(str3).equal('Task||__key__>TasksomeTask||-1|-1||__key__||');
+            expect(str2).equal(
+                `User${separator + separator}name=john${separator +
+                    separator}-1${separator}-1${separator}phone+${separator + separator + separator}`
+            );
+            expect(str3).equal(
+                `Task${separator + separator}__key__>Task${separator}someTask${separator +
+                    separator}-1${separator}-1${separator + separator}__key__${separator + separator}`
+            );
         });
     });
 });
